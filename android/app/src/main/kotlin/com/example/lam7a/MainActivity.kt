b0bcffa5ca3f64133e7c466e335d68bb4a89387e
KarimZakzouk <karimzakzouk69@gmail.com>
2025-10-05 21:31:14 +0300

    try

---
+++ android/app/src/main/kotlin/com/example/lam7a/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lam7a
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/core/utils.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+String rgbToHex(Color color) {
+  return '${color.red.toRadixString(16).padLeft(2, '0')}${color.green.toRadixString(16).padLeft(2, '0')}${color.blue.toRadixString(16).padLeft(2, '0')}';
+}
+
+Color hexToColor(String hex) {
+  return Color(int.parse(hex, radix: 16) + 0xFF000000);
+}
+
+void showSnackBar(BuildContext context, String content) {
+  ScaffoldMessenger.of(context)
+    ..hideCurrentSnackBar()
+    ..showSnackBar(
+      SnackBar(
+        content: Text(content),
+      ),
+    );
+}
